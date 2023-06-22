17dcfe20c26261db00c887d4f3dc39085b9c1d11
Metal <50769245+Metal-666@users.noreply.github.com>
2023-06-22 12:55:13 +0300

    Made login screen fancier :3

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -15,7 +15,8 @@ class LoginPage extends HookWidget {
 
     return pageRoot(
       loginForm(
-        loginCodeInput(loginCodeController),
+        label(context),
+        loginCodeInput(context, loginCodeController),
         submitButton(context, loginCodeController),
       ),
     );
@@ -24,34 +25,71 @@ class LoginPage extends HookWidget {
   Widget pageRoot(final Widget loginForm) => Center(child: loginForm);
 
   Widget loginForm(
+    final Widget label,
     final Widget loginCodeInput,
     final Widget submitButton,
   ) =>
-      Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          loginCodeInput,
-          submitButton,
-        ],
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Expanded(child: SizedBox()),
+            label,
+            loginCodeInput,
+            const Expanded(child: SizedBox()),
+            Center(
+              child: submitButton,
+            ),
+          ],
+        ),
       );
 
-  Widget loginCodeInput(final TextEditingController controller) => Row(
-        children: [
-          Pinput(
-            length: constants.loginCodeLength,
-            controller: controller,
+  Widget label(final BuildContext context) => Text(
+        'Login Code:',
+        style: Theme.of(context).textTheme.headlineMedium,
+      );
+
+  Widget loginCodeInput(
+    final BuildContext context,
+    final TextEditingController controller,
+  ) =>
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Pinput(
+          length: constants.loginCodeLength,
+          controller: controller,
+          defaultPinTheme: PinTheme(
+            padding: const EdgeInsets.all(15),
+            textStyle: const TextStyle(fontSize: 20),
+            decoration: BoxDecoration(
+              color: ElevationOverlay.applySurfaceTint(
+                Theme.of(context).cardColor,
+                Theme.of(context).colorScheme.surfaceTint,
+                5,
+              ),
+            ),
           ),
-        ],
+        ),
       );
 
   Widget submitButton(
     final BuildContext context,
     final TextEditingController loginCodeController,
   ) =>
-      TextButton(
+      OutlinedButton(
         onPressed: () => context
             .read<root.Bloc>()
             .add(root.SubmitLoginCode(loginCodeController.text)),
-        child: const Text('Submit'),
+        style: OutlinedButton.styleFrom(
+          side: BorderSide(
+            width: 2,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+        child: const Text(
+          'Submit',
+          style: TextStyle(fontSize: 22),
+        ),
       );
 }
